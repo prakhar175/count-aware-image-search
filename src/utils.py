292dd0c9bfe26f1d002fc437f05a4a02c0d541d0
prakhar-tecnod8 <prakhar.kothari@tecnod8.com>
2026-02-02 02:02:06 +0530

    storing metadata.json

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import json
+def ensure_processed_images(raw_path):
+    raw_path=Path(raw_path)
+    processed_path = raw_path.parent.parent / "processed" / raw_path.name
+    processed_path.mkdir(parents=True, exist_ok=True)
+    return processed_path
+def save_metadata(metadata, raw_path):
+    processed_path=ensure_processed_images(raw_path)
+    output_path=processed_path / "metadata.json"
+    with open (output_path, 'w') as F:
+        json.dump(metadata,F)
+    return output_path
