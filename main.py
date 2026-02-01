106c2287de9ac08a5b9669736202ccd8a8ef53cd
prakhar-tecnod8 <prakhar.kothari@tecnod8.com>
2026-02-01 20:38:54 +0530

    framework of project

---
+++ main.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import time
+from pathlib import Path
+
+def init_session_state() -> None:
+    session_defaults = {
+        "image_path":"path"
+    }
+    
+    for key,value in session_defaults.items():
+        if key not in st.session_state:
+            st.session_state[key]=value
+            
+init_session_state()
+
+st.set_page_config(page_title="Smart Image searcher!", page_icon="📄", layout="wide")
+st.title("Computer Vision based Image finder", text_alignment="center")
+
+option = st.radio("Choose the option",
+                  ( "Process the new images", "Load existing"),
+                  horizontal=True)
+
+if option=="Process the new images":
+    st.expander("Process new images", expanded=True)
+    col1,col2 = st.columns(2)
+    with col1:
+        image_dir=st.text_input("Image directory path:", placeholder="path ")
+    with col2:
+        model_path=st.text_input("Model weight path",placeholder=" eg - yolo11m.pt")
+        
+    if st.button("Start Inference"):
+        if image_dir:
+            try :
+                with st.spinner("COOKING...."):
+                    st.success("DONE!!!!")
+            except Exception as e:
+                st.error("Error :", str(e))
+        else:
+            st.warning("Enter the path you dumbo")
+else:
+    with st.expander("Load Exisiting Metadata", expanded=True):
+        m_path=st.text_input("Metadata file path:", placeholder="path/to/metadata.json")
+        
+        if st.button("Load Metadata"):
+            if m_path:
+                try:
+                    with st.spinner("Running obj detection"):
+                        time.sleep(3)
+                        st.success("Successfllu loaaded")
+                except Exception as e:
+                    st.error("Error", e)
+            else:
+                st.warning("Path daal be")
