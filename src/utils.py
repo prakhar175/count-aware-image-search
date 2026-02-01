6cdd3a7e195ac1b707863cecdbe7b167107e6878
prakhar175 <prakhar.k2004@gmail.com>
2026-02-02 02:11:35 +0530

    changes

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 import json
+
 def ensure_processed_images(raw_path) -> Path:
     raw_path=Path(raw_path)
     processed_path = raw_path.parent.parent / "processed" / raw_path.name
     processed_path.mkdir(parents=True, exist_ok=True)
     return processed_path
+
 def save_metadata(metadata, raw_path) -> Path:
     processed_path=ensure_processed_images(raw_path)
     output_path=processed_path / "metadata.json"
